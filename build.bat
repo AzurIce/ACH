c0ca63c6daec12f4dd506d2dafffbb23e4bcc751
Azur冰弦 <973562770@qq.com>
2022-06-10 01:18:40 +0800

    update

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 statik -src=assets/dist/ -f
 set GOOS=linux
 set GOARCH=amd64
-go build -o Builds/v1.0/ACH-1.0.0alpha4 ach
+go build -o Builds/v1.0/ACH-1.0.0alpha5 ach
 set GOOS=windows
-go build -o Builds/v1.0/ACH-1.0.0alpha4.exe ach
+go build -o Builds/v1.0/ACH-1.0.0alpha5.exe ach
 pause
