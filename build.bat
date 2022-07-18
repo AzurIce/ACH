84f202b163915e036ac8eca6717326dc2993e4da
Azur冰弦 <973562770@qq.com>
2022-07-18 22:30:44 +0800

    Fixed cmdChan stucked bug

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 statik -src=./assets/dist -f
 set GOOS=linux
 set GOARCH=amd64
-go build -o Builds/v1.0/ACH-1.0.0alpha5 ach
+go build -o Builds/v1.0/ACH-1.0.0alpha6 ach
 set GOOS=windows
-go build -o Builds/v1.0/ACH-1.0.0alpha5.exe ach
+go build -o Builds/v1.0/ACH-1.0.0alpha6.exe ach
 pause
