c332c7ffb6e2d31c0e964f5cdef8f0dcf835c262
Azur冰弦 <973562770@qq.com>
2022-07-29 19:09:26 +0800

    feat: user register from admin manage page

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 statik -src=./assets/dist -f
 set GOOS=linux
 set GOARCH=amd64
-go build -o Builds/v1.0/ACH-1.0.0alpha9 ach
+go build -o Builds/v1.0/ACH-1.0.0alpha10 ach
 set GOOS=windows
-go build -o Builds/v1.0/ACH-1.0.0alpha9.exe ach
+go build -o Builds/v1.0/ACH-1.0.0alpha10.exe ach
 pause
